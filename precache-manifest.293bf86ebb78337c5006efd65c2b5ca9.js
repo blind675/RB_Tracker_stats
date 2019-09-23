b27705598e1980077bcb85115032a37f45df9fb9
Catalin-Andrei BORA <catalin.bora@gmail.com>
2019-09-23 22:53:06 +0300

    Updates

---
+++ precache-manifest.293bf86ebb78337c5006efd65c2b5ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8b04189070b7dd3b066a4a920fb420",
+    "url": "/RB_Tracker_stats/index.html"
+  },
+  {
+    "revision": "e4eac94ccc3567e86ff4",
+    "url": "/RB_Tracker_stats/static/css/2.d64b8b57.chunk.css"
+  },
+  {
+    "revision": "343caa710ce1921bff9f",
+    "url": "/RB_Tracker_stats/static/css/main.b08be5e8.chunk.css"
+  },
+  {
+    "revision": "e4eac94ccc3567e86ff4",
+    "url": "/RB_Tracker_stats/static/js/2.cd8887d1.chunk.js"
+  },
+  {
+    "revision": "343caa710ce1921bff9f",
+    "url": "/RB_Tracker_stats/static/js/main.1597ec52.chunk.js"
+  },
+  {
+    "revision": "e3763342c44100a593c8",
+    "url": "/RB_Tracker_stats/static/js/runtime~main.85875d7d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RB_Tracker_stats/precache-manifest.51d26c8d5df4b9f081feba4fc4b3d1c1.js"
+  "/RB_Tracker_stats/precache-manifest.293bf86ebb78337c5006efd65c2b5ca9.js"
 );
 
 self.addEventListener('message', (event) => {
